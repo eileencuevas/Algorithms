a23c0e35951ef8adac51b4c7af875ca2dcbafb64
steelworx <eileencuevas62@gmail.com>
2019-03-26 21:33:26 -0400

    second pass implementation of making_change()

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,15 +3,29 @@
 import sys
 
 
-def making_change(amount, denominations=[1, 5, 10, 25, 50]):
-    if amount < 0:
-        return 0
-    elif amount == 0:
-        return 1
-    elif not denominations:
-        return 0
-    else:
-        return making_change(amount, denominations[:-1]) + making_change(amount - denominations[-1], denominations)
+def making_change(amount, denominations, cache=None):
+    # first pass attempt:
+
+    # if amount < 0:
+    #     return 0
+    # elif amount == 0:
+    #     return 1
+    # elif not denominations:
+    #     return 0
+    # else:
+    #     return making_change(amount, denominations[:-1]) + making_change(amount - denominations[-1], denominations)
+
+    # second pass attempt, with cache
+    if not cache:
+        cache = {int: 0 for int in range(amount + 1)}
+        cache[0] = 1
+
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            difference = higher_amount - coin
+            cache[higher_amount] += cache[difference]
+
+    return cache[amount]
 
 
 if __name__ == "__main__":
